b5483a14b4a8a688c8505c5f04858166150dd925
oriAmrani128 <121358023+oriAmrani128@users.noreply.github.com>
2023-07-18 13:15:26 +0300

    change3

--- Ori.java
+++ Ori.java
@@ -1,7 +1,7 @@
 public class Ori {
 public static void main (String[] args){
 
-System.out.println("Ori !!");
+System.out.println("Ori !!!");
 System.out.println("Ori !!");
 System.out.println("Ori !!");
 System.out.println("Ori !!");
